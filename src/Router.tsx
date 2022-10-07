a026f49e2411932213a8d0628a8c7512587e19ea
asuraCoder <asuracoder@gmail.com>
2022-10-08 00:36:23 +0900

    done2

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,7 +4,7 @@ import Coin from "./routes/Coin";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Coins />}></Route>
         <Route path="/:coinId/*" element={<Coin />}></Route>
